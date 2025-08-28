2e1da7bbb546b880038ec3230fb5cf632a847b2d
Khair Ul Wara <mynameiskhairulwara@gmail.com>
2025-08-28 10:57:45 +0500

    commit

--- app.js
+++ app.js
@@ -1,73 +1,45 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
-app.listen(3000);
-//listening on port 3000
-//sending a response
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
-//register view engine
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://Khairulwara:test123@node-practice.yqodzt4.mongodb.net/node-practice?retryWrites=true&w=majority&appName=Cluster0'
+
+
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
 app.set('view engine', 'ejs');
-//using 3rdparty middleware to log data
-app.use(morgan('dev'));
-//creating middleware to make our static files public and available
+
+// middleware & static files
 app.use(express.static('public'));
-//creating middleware for logging request data
-app.use((req,res,next) =>{
-
-    console.log('new request made:');
-    console.log('host:',req.hostname);
-    console.log('path:',req.path);
-    console.log('method:',req.method);
-    next();
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
-//rendering a view
+
+// routes
 app.get('/', (req, res) => {
-    const blog = [
-        { title: 'Blog 1', snippet: 'Content of Blog 1' },
-        { title: 'Blog 2', snippet: 'Content of Blog 2' },
-        { title: 'Blog 3', snippet: 'Content of Blog 3' }
-    ];
-
-
-    res.render('index', { title: 'Home Page',blog: blog }); });
+  res.redirect('/blogs');
+});
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About Us' });
-}
-);
-app.get('/about-us', (req, res) => {
-    res.redirect('/about', 301); // Redirecting to /about with a 301 status code
-}
-);
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create Blog' });
-}
-);
-//404 page
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404 - Page Not Found' });
-}
-);
-
-// This code sets up a basic Express server that listens on port 3000 and handles requests to the root and about pages, as well as redirects and 404 errors.
-
-
-//method to show html page without using ejs
-// app.get('/about', (req, res) => {    
-//     res.sendFile('./views/about.html', { root: __dirname });    
-
-// }
-// );
-// //redirecting
-// app.get('/about-me', (req, res) => {
-//     res.redirect('/about');
-// }
-// );
-// //404 page
-// app.use((req, res) => {
-//     res.status(404).sendFile('./views/404.html', { root: __dirname });
-// }
-// );
-//console.log(__dirname); // This will log the current directory path
-console.log('listening for requests on port 3000');
-// This code sets up a basic Express server that listens on port 3000 and handles requests to the root and about pages, as well as redirects and 404 errors.
+  res.status(404).render('404', { title: '404' });
+});
